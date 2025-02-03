75f711118c020c27cf064ecc80ad202619900735
Ramis Shakurov <rama-shakurov@yandex.ru>
2025-02-03 19:42:51 +0500

    try

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+'use strict';
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
@@ -7,3 +8,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+function renderCard(link, name) {
+    const template = document.querySelector('#card-template').content;
+    const templateClone = template.cloneNode(true);
+    const templateImage = templateClone.querySelector('.card__image');
+    const templateTitle = templateClone.querySelector('.card__title');
+    const listOfCards = document.querySelector('.places__list');
+    const buttonDelete = templateClone.querySelector('.card__delete-button');
+
+    templateImage.src = link;
+    templateTitle.textContent = name;
+    listOfCards.append(templateClone);
+
+    buttonDelete.addEventListener('click', evt => {
+        evt.target.closest(".card__delete-button").parentElement.remove();
+    });
+
+}
+
+for (const i of initialCards) {
+    renderCard(i.link, i.name);
+}
